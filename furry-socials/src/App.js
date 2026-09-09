0b861d7e350de649c38b04b9c5283668e70afa48
Mathieu <mathieuvantuil2000@gmail.com>
2026-09-09 20:00:33 +0200

    Json connected and working on profile

--- furry-socials/src/App.js
+++ furry-socials/src/App.js
@@ -1,22 +1,20 @@
+//css import
 import "./App.css";
+//hook import
+import { useProfileData } from "./hooks/useProfileData";
+
+//component imports
+import Profile from "./components/profile/profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="./profile/Profile-picture.png"
-          className="Profile-picture"
-          alt=""
-        />
-        <h1>MrMatt</h1>
-        <h2>
-          the <b>oversized house cat</b>
-        </h2>
-        <sub>#MrMatt04</sub>
-      </header>
-    </div>
+  const { data, loading, error } = useProfileData(
+    `${process.env.PUBLIC_URL}/profile.json`
   );
+
+  if (loading) return <div>Loading...</div>;
+  if (error || !data) return <div>Error loading profile data</div>;
+
+  return <Profile profile={data.profileData} />;
 }
 
 export default App;
---
+++ furry-socials/src/components/profile/profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Profile({ profile }) {
+  return (
+    <div>
+      <h1>hi there {profile.name}</h1>
+    </div>
+  );
+}
+
+export default Profile;
